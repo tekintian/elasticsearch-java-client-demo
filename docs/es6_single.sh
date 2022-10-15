07dc1847ddc331f35ed728b3a6b2d692b431e152
Tekin Tian <Tekin@MacdeMacBook-Pro-352.local>
2022-10-15 19:37:12 +0800

    fix es single node

--- docs/es6_single.sh
+++ docs/es6_single.sh
@@ -7,6 +7,7 @@ docker run -itd --name elasticsearch6 -p 9200:9200 -p 9300:9300 \
     -v $PWD/es6.5_config/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
     -v $PWD/es6.5_config/jvm.options:/usr/share/elasticsearch/config/jvm.options \
     -v $PWD/data:/usr/share/elasticsearch/data \
+    -e "discovery.type=single-node" \
     elasticsearch:6.5.4
 
 # check the es logs
